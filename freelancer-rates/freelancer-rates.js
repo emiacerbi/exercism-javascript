5808fe91832f07b67bad26939bfe3fcf0159e424
Emiliano Acerbi <eacerbip@gmail.com>
2022-06-09 12:49:51 -0300

    freelancer-rates

---
+++ freelancer-rates/freelancer-rates.js
@@ -0,0 +1,49 @@
+// @ts-check
+
+/**
+ * The day rate, given a rate per hour
+ *
+ * @param {number} ratePerHour
+ * @returns {number} the rate per day
+ */
+export function dayRate(ratePerHour) {
+  return ratePerHour * 8
+}
+
+/**
+ * Calculates the number of days in a budget, rounded down
+ *
+ * @param {number} budget: the total budget
+ * @param {number} ratePerHour: the rate per hour
+ * @returns {number} the number of days
+ */
+export function daysInBudget(budget, ratePerHour) {
+  return Math.floor(budget / dayRate(ratePerHour))
+}
+
+/**
+ * Calculates the discounted rate for large projects, rounded up
+ *
+ * @param {number} ratePerHour
+ * @param {number} numDays: number of days the project spans
+ * @param {number} discount: for example 20% written as 0.2
+ * @returns {number} the rounded up discounted rate
+ */
+export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+  const months = Math.floor(numDays / 22)
+  const remainingDays = numDays % 22
+
+  return Math.ceil(
+    applyDiscount(months * 22 * dayRate(ratePerHour), discount) +
+    remainingDays * dayRate(ratePerHour)
+  )
+}
+
+/**
+ * @param {number} value
+ * @param {number} percentage
+ * @returns {number}
+ */
+const applyDiscount = (value, percentage) => {
+  return Math.ceil(value * (1 - percentage))
+}

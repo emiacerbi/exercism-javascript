a81b7be558c7b84cb8a125e3604a6de512eec896
Emiliano Acerbi <eacerbip@gmail.com>
2022-06-12 12:46:44 -0300

    lucky-numbers

---
+++ lucky-numbers/lucky-numbers.js
@@ -0,0 +1,41 @@
+// @ts-check
+
+/**
+ * Calculates the sum of the two input arrays.
+ *
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @returns {number} sum of the two arrays
+ */
+export function twoSum(array1, array2) {
+  const firstValue = array1.map(el => el).join('')
+  const secondValue = array2.map(el => el).join('')
+  return Number(firstValue) + Number(secondValue)
+}
+
+/**
+ * Checks whether a number is a palindrome.
+ *
+ * @param {number} value
+ * @returns {boolean}  whether the number is a palindrome or not
+ */
+export function luckyNumber(value) {
+  return Number(value.toString().split('').reverse().join('')) === value
+}
+
+/**
+ * Determines the error message that should be shown to the user
+ * for the given input value.
+ *
+ * @param {string|null|undefined} input
+ * @returns {string} error message
+ */
+export function errorMessage(input) {
+  if(!input) {
+    return 'Required field'
+  } else if(Number(input) === 0 || !Number(input)) {
+    return 'Must be a number besides 0'
+  } else {
+    return ''
+  }
+}
